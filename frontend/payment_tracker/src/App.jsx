4149763a7a759268a217e91df43f475add1cd07b
Sammon04 <samuel.belanger19@gmail.com>
2026-05-01 20:23:04 -0400

    added home page and simple nav bar on each page

--- frontend/payment_tracker/src/App.jsx
+++ frontend/payment_tracker/src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from './components/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Payment Tracker</h1>
+      <div>
+        <p>Click here to log in:</p>
+        <button onClick={() => navigate("/login")}>Login</button>
+      </div>
+      <div>
+        <p>Need an account? Click here to register:</p>
+        <button onClick={() => navigate("/register")}>Register</button>
+      </div>
+    </div>
+  )
+}
 
 function App() {
-  
   return (
+
     <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </nav>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={
@@ -18,6 +41,7 @@ function App() {
         } />
       </Routes>
     </BrowserRouter>
+
   );
 
 }
